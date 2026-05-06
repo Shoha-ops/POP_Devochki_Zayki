d779a88cc5286ffda423ec229050a7e2a935400b
viktordrn <victordrn40@gmail.com>
2026-05-06 23:00:39 +0500

    Add files via upload

---
+++ Data Storage.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+void saveOrders(const vector<Order>& orders);
+void loadOrders(vector<Order>& orders);
+
